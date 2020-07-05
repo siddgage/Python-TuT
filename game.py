7f683bfaa83bd014cc7c82786f42baa1c6f1e700
SIDD <siddgage@gmail.com>
2020-07-05 14:50:06 +0530

    plability improved with other bug fixes, result calc is remaining

--- game.py
+++ game.py
@@ -48,7 +48,7 @@ def display_board():
 #display for current player only
 def display_board_for_current_player():
         print()
-        print(f"******\nDashbord for team {current_team}")
+        print(f"******\nDashbord for team {current_team+1}")
         print(f"Score is: {Score[current_team]} | Wicket: {Wicket[current_team]} | Overs: {round(Over[current_team])} | No of player remained: {No_of_teams[current_team]}\n******")
         print()
 
@@ -56,14 +56,17 @@ def display_board_for_current_player():
 # Game Start here
 def ball_thrown():
     global Over,Run_Scored,current_team
+    
     #Randmonly Generate a Value Betwwen 0 and 6 for the runs
     Run_Scored = rd.randint(0, 6)
     #Iterating Over
     Over[current_team] +=0.1
-    #Checking Conditions 
-    check_wicket_or_add_run()
-    check_over()
-    can_continue()
+    #checking condition
+    if check_over() == "over":
+        pass
+    else: 
+        check_wicket_or_add_run()
+        can_continue()
 
 def check_wicket_or_add_run():
     global No_of_teams, run, Wicket,current_team
@@ -77,15 +80,15 @@ def check_wicket_or_add_run():
         run += Run_Scored 
         #Fixed where score was not added in runtime
         Score[current_team] =run
-        print(f"\nScore:\n{Run_Scored} added. Total is {run}")
+        print(f"Score:\n{Run_Scored} added. Total is {run}")
 
 #Number of Over Are Checked
 def check_over():
     global Over, totalOver,current_team
     if Over[current_team] > totalOver:
-        display_board_for_current_player()
         # sys.exit("\nMax Over reached.") 
-        return -1
+        check_current_team()
+        return "over"
     #value = Over % 1 
     #This gives the decimal place value
     #Then Rounded using Round Func
@@ -94,60 +97,47 @@ def check_over():
         #Since Over are of 6 balls, Remaning are adjusted
         Over[current_team] += .4 
         print(f"Next Over...{round(Over[current_team])}\n")
-        return 1
     else:
         #Print the remaining Balls in the over
         print(f"{round(0.6 - round(Over[current_team] % 1,1),1)} balls remaining..")
         print()
-        return 1
 
 #Checks Wicket
 def can_continue():
     if Wicket == 10:
         display_board()
         # sys.exit("\nMatch Over...All wickets are taken")
-        return -1
-    else: return 1
+        check_current_team()
+    else: pass
 
 #Check Current Player
-def check_current_player():
-    global current_team
+def check_current_team():
+    global current_team, run
     #Check the Return Value
-    if check_over() and can_continue() == 1:
-        pass
-    elif check_over() and can_continue() == -1:
-        current_team += 1
-        # Score.append(run)
-        print(f"PLAYER CHANGE TO TEAM {current_team}")
-       # reset_stats()
+    check_winner()
+    display_board_for_current_player()
+    current_team += 1
+    run =0
+    # Score.append(run)
+    print(f"TEAM CHANGE TO TEAM {current_team +1}")
+    # reset_stats()
 
 def check_winner():
     global Score, teams, current_team
     #check if the last team is playing
     winner = teams[0] 
-    if teams[current_team] == teams[no_of_teams-1]:
-        for i in  range(no_of_teams):
-            if Score[i+1] > Score[i]:
+    if teams[current_team] == len(teams)-1:
+        for i in  range(len(teams)-1):
+            if Score[i+1] >= winner:
                 winner = teams[i+1]
-                if Score[i+1] == Score[i]:
-                    print(f"Tied Between {teams[i +1]} and {teams[i+2]} ")
-        print(f"*********\n\nTeam {winner} Wins the match. Congrats\n\n*********")
+                # if Score[i+1] == Score[i]:
+                #     print(f"Tied Between {teams[i]} and {teams[i+11]} ")
+        display_board()
+        print(f"*********\n\nTeam {winner +1} Wins the match. Congrats\n\n*********")
         sys.exit("Thanks for playing...\n")
-    else: pass
-                    
-  
-
-
-# def reset_stats():
-#     global run, Wicket, Over, No_of_teams,Run_Scored
-#     run = 0
-#     Wicket = 0
-#     Over = 0.0
-#     No_of_teams = 11
-#     Run_Scored = 0
+    else: print(f"Team {current_team+1} chance is over")
 
 #  Execution start from here
-
 if __name__ == "__main__":
 
     no_of_teams = int(input("Enter the number of teams: "))

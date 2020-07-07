82d2be08af0efc08035132dca9040adf8013a030
SIDD <siddgage@gmail.com>
2020-07-07 16:44:26 +0530

    many improvements

--- cricket.py
+++ cricket.py
@@ -1,16 +1,18 @@
 import random as rd
 import sys
-# Global Variables
 
+# Global Variables
 # List of number of teams
 Teams = []
 current_team = 0
 index = 0
+
 # Score 
 Score = []
 Run_Tracker = [current_team][index]
 run = 0
 Run_Scored = 0
+Run_Tracker = []
 
 #Wicket
 Wicket = []
@@ -18,69 +20,74 @@ No_of_Wickets_Remaining = []
 
 #Over
 Over = []
+No_of_Balls = 0
+No_of_Balls_for_team = []
 
 # Main Function
 def play_game():
-    # For keeping the loop infinitely
-    Playing =True
     # Do While Loop
-    while Playing:
+    while True:
         
         print("\n********************************************************************\n")
-        ch = input("1.Show Dashboard for current Team\n2.Show Dashboard for all Teams\n3.Show the Run Tracker\n4.Throw the ball\n5.Exit\nEnter Your Choice: ")
+        ch = int(input("1.Show Dashboard\n2.Show Run Tracker\n3.Throw the ball\n4.Exit\nEnter Your Choice: "))
         print("\n********************************************************************\n")
         # Switch Case 
-        if ch == "1":
-            display_board_for_current_player()
-        elif ch == "2":
-            display_board()
-        elif ch=="3":
-            display_run_tracker()
-        elif ch == "4":
+        if ch == 1:
+            select_dashboard()
+        elif ch == 2:
+            display_run_tracker_for_current_team()
+        elif ch==3:
             ball_thrown()
-        elif ch == "5":
+        elif ch == 4:
             sys.exit("\nSee Your Next Time...\n")
         else:
-            print("Wrong Choice, try again...")   
+            print("\nWrong Choice, try again...")   
+
+def select_dashboard():
+    print("\n$  $  $  $  $  $  $  $ $  $  $  $  $  $  $  $  $  $  $  $\n")
+    print("1.Show Dashboard for current Team\n2.Show Dashboard for all Teams\n3.Exit")
+    ch = int(input("Enter Your Choice: "))
+    print("\n$  $  $  $  $  $  $  $ $  $  $  $  $  $  $  $  $  $  $  $\n")
+    if ch == 1:    
+        display_board_for_current_team()
+    elif ch == 2:
+        display_board()
+    elif ch == 3:
+        print("Back to main menu")
+        return
+    else:
+        print("\nWrong Choice Try Again")
+        select_dashboard()
 
 # Display For all team
 def display_board():
+    print("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *")
     for i in range(no_of_teams):
-        print()
-        print(f"******\nDashbord for team {i+1}")
-        print(f"Score is: {Score[i]} | Wicket: {Wicket[i]} | Overs: {round(Over[i],1)} | No of player remained: {No_of_Wickets_Remaining[i]}\n******")
-        print()
+        print(f"\t\t\t\t\t\tDashbord for team {i+1}")
+        print(f"\t\t\t\t\t\tScore is: {Score[i]} | Wicket: {Wicket[i]} | Overs: {round(Over[i],1)} | No of player remained: {No_of_Wickets_Remaining[i]}\n\n")
+    print("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *")
+
 #display for current player only
-def display_board_for_current_player():
-        print()
-        print(f"******\nDashbord for team {current_team+1}")
-        print(f"Score is: {Score[current_team]} | Wicket: {Wicket[current_team]} | Overs: {round(Over[current_team],1)} | No of player remained: {No_of_Wickets_Remaining[current_team]}\n******")
-  
-        print()
+def display_board_for_current_team():
+        print("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *")
+        print(f"\t\t\t\t\t\tDashbord for team {current_team+1}")
+        print(f"\t\t\t\t\t\tScore is: {Score[current_team]} | Wicket: {Wicket[current_team]} | Overs: {round(Over[current_team],1)} | No of player remained: {No_of_Wickets_Remaining[current_team]}\n")
+        print("*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * *  *  *  *  *  *  *  *  *  *  *  *  *  *  *")
 
-# Not Working
-
-def display_run_tracker():
-    # score = []
-    # print(f"Runs for the Team {current_team + 1}: ")
-    # for i in range(len(Run_Tracker[current_team])-1):
-    #     print(f"{i}" + ", ")[:-2]
-    # for i in range(index): 
-    #     score.append(Run_Tracker[current_team][i]) # +  ", "
-
-    # print(score)
-    # for i in score:
-    #     print(i)[:-1]
-    pass
-
+def display_run_tracker_for_current_team(): 
+    print(f"Run Tracker for Team {current_team +1}")
+    print(*Run_Tracker[current_team], sep= ", ")
+            
 # Game Start here
 def ball_thrown():
-    global Over,Run_Scored,current_team
+    global Over,Run_Scored,current_team, No_of_Balls
     
     #Randmonly Generate a Value Betwwen 0 and 6 for the runs
     Run_Scored = rd.randint(0, 6)
     #Iterating Over
     Over[current_team] +=0.1
+    No_of_Balls +=1
+
     #checking condition
     check_run()
     check_wicket()
@@ -93,22 +100,24 @@ def ball_thrown():
 def check_run():
     global No_of_Wickets_Remaining, run, Wicket,current_team, index, Run_Tracker
     # When score is 0, Player is Out
+    index = 0
     if Run_Scored == 0:
         print("\nThats a wicket, ouch!!!")
+        #Tracking the Runs
+        Run_Tracker[current_team].append('W')
+
         No_of_Wickets_Remaining[current_team] -= 1
         Wicket[current_team] +=1
     #Else Runs Are Added To the Score
     else:
         run += Run_Scored 
         #Tracking the Runs
-        if Run_Scored == 0:
-            Run_Tracker = "W"
-        else:
-            Run_Tracker = Run_Scored
-        index += 1
+        Run_Tracker[current_team].append(Run_Scored)
+
         #Fixed where score was not added in runtime
-        Score[current_team] =run
+        Score[current_team] = run
         print(f"Score:\n{Run_Scored} added. Total is {run}")
+    index +=1
 
 #Number of Over Are Checked
 def check_over():
@@ -132,20 +141,23 @@ def check_over():
 #Checks Wicket
 def check_wicket():
     if Wicket == 10:
-        display_board()
+        display_run_tracker_for_current_team()
+        display_board_for_current_team()
         check_current_team()
 
 #Check Current Player
 def check_current_team():
-    global current_team, run
-    #Check the Return Value
+    global current_team, run, No_of_Balls_for_team
+    #Set Total No of Balls Played 
+    No_of_Balls_for_team[current_team] = No_of_Balls
+
     check_winner()
-    display_board_for_current_player()
+    display_run_tracker_for_current_team()
+    display_board_for_current_team()
     current_team += 1
-    run =0
-    # Score.append(run)
-    print(f"### TEAM CHANGE TO TEAM {current_team +1} ###")
-    # reset_stats()
+    run = 0
+
+    print(f"#  #  # TEAM CHANCE TO TEAM {current_team +1} #  #  #")
 
 def check_winner():
     global Score, Teams, current_team
@@ -165,23 +177,30 @@ def check_winner():
                 if Score[i] == Score[j+1]:
                     print(f"Tied Between {Teams[i+1]} and {Teams[j+1]} ")
             break
-        print(f"*********\n\nTeam {winner +1} Wins the match with the score of {winner_score}. Congrats\n*********")
+
+        print(f"*  *  *  *  *  *  *  *  *\n\nTeam {winner +1} Wins the match with the score of {winner_score}. Congrats\n*  *  *  *  *  *  *  *  *")
         sys.exit("Thanks for playing...\n")
-    else: print(f"### TEAM {current_team+1} CHANGE OVER ###")
+    else: print(f"#  #  # TEAM {current_team+1} CHANGE OVER #  #  #")
+
+def appendFunc():
+    #  To over come index out of range
+    for i in range (no_of_teams):
+        Teams.append(i)
+        Score.append(0)
+        Run_Tracker.append([])
+        Wicket.append(0)
+        Over.append(0)
+        No_of_Wickets_Remaining.append(10)
+        No_of_Balls_for_team.append(0)
+        No_of_Wickets_Remaining.append(10)
 
 #  Execution start from here
 if __name__ == "__main__":
 
     no_of_teams = int(input("Enter the number of Teams: "))
-    totalOver = int(input("\nEnter the number of Overs: "))
-    totalOver -= 1
-    #  To over come index out of range
-    for i in range (no_of_teams):
-        Teams.append(i)
-        Score.append(0)
-        Wicket.append(0)
-        Over.append(0)
-        No_of_Wickets_Remaining.append(10)
+    totalOver = int(input("\nEnter the number of Overs: ")) -1
 
+
+    appendFunc()
     display_board()
     play_game()
